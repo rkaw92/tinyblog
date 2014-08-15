b9b7eaba4b7984b54b36c22f1f0b9e4780867e32
Robert Kawecki <robert+github@rkaw.pl>
2014-08-15 18:15:45 +0200

    Abstracted away the storage/index/factory concerns and hid them under a common Repository interface. A mongodb-like cursor object is returned in lieu of actual results, supporting chaining query criteria during repository calls. Also reworked the main server file so that it uses the new interface.
    The sorting code remains largely untested.

--- server.js
+++ server.js
@@ -2,13 +2,16 @@ var BlogEngine = require('./lib/BlogEngine').BlogEngine;
 var FileStorage = require('./lib/storage/FileStorage').FileStorage;
 var MemoryIndex = require('./lib/index/MemoryIndex').MemoryIndex;
 var YAMLPostFactory = require('./lib/factories/YAMLPostFactory').YAMLPostFactory;
+var FileRepository = require('./lib/repositories/FileRepository').FileRepository;
 var log = require('./lib/log');
 
-var storage = new FileStorage(__dirname + '/posts');
-var index = new MemoryIndex();
+var postStorage = new FileStorage(__dirname + '/posts');
+var postIndex = new MemoryIndex();
 var postFactory = new YAMLPostFactory();
-var blog = new BlogEngine(storage, postFactory, index, null);
+var postRepository = new FileRepository('Post', ['ID', 'author', 'date', 'title', 'language'], postStorage, postIndex, postFactory);
+
+var blog = new BlogEngine(postRepository);
 
 blog.serveBlog('127.0.0.1', 4067).done(function(){
-	log.info('Blog engine started!');
+	log.info('Blog engine started! Listening on http://127.0.0.1:4067');
 });
