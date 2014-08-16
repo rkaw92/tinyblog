3d8b878398fbb2753a5216185a90f4aaf093a8f3
Robert Kawecki <robert+github@rkaw.pl>
2014-08-16 03:03:05 +0200

    Added support for template engines. The blog engine now uses a dependency-injected renderer for displaying posts.

--- server.js
+++ server.js
@@ -3,14 +3,18 @@ var FileStorage = require('./lib/storage/FileStorage').FileStorage;
 var MemoryIndex = require('./lib/index/MemoryIndex').MemoryIndex;
 var YAMLPostFactory = require('./lib/factories/YAMLPostFactory').YAMLPostFactory;
 var FileRepository = require('./lib/repositories/FileRepository').FileRepository;
+
+var JuicerTemplateEngine = require('./lib/templateEngines/JuicerTemplateEngine').JuicerTemplateEngine;
+
 var log = require('./lib/log');
 
 var postStorage = new FileStorage(__dirname + '/posts');
 var postIndex = new MemoryIndex();
 var postFactory = new YAMLPostFactory();
 var postRepository = new FileRepository('Post', ['ID', 'author', 'date', 'title', 'language'], postStorage, postIndex, postFactory);
+var templateEngine = new JuicerTemplateEngine(__dirname + '/templates');
 
-var blog = new BlogEngine(postRepository);
+var blog = new BlogEngine(postRepository, templateEngine);
 
 blog.serveBlog('127.0.0.1', 4067).done(function(){
 	log.info('Blog engine started! Listening on http://127.0.0.1:4067');
